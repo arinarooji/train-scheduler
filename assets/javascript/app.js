00ed75481dcfbe23a03c4b91d1b71022e5d5f24c
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-14 20:26:39 -0400

    updated html, js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,28 @@
-console.log("JavaScript enabled");
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDULhMRgwWmnuikc1SEhioUHTgdRhMZYy0",
+    authDomain: "kaa-train-schedule.firebaseapp.com",
+    databaseURL: "https://kaa-train-schedule.firebaseio.com",
+    projectId: "kaa-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "731375124025"
+  };
+  firebase.initializeApp(config);
+//Create a variable to reference the database
+var rootRef = firebase.database().ref();
+var trainsRef = rootRef.child("trains");
+var newTrainRef = trainsRef.push();
+
+//Get input from the user, ensuring all values entered (input)
+$(".btn").on("click", function(){
+    if ($("input").val().trim() !== ""){
+        //Push the new train data to the server
+        newTrainRef.set({
+            name: $("#name-input").val(),
+            destination: $("#name-input").val(),
+            initialTime: $("#time-input").val(),
+            frequency: $("#rate-input").val()
+        });
+    }
+        
+});
