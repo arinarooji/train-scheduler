fae18f5a3e2a5a3b99b104199229dc92563d52af
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-27 17:29:00 -0400

    JS update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,13 +6,23 @@ var config = {
     projectId: "kaa-train-schedule",
     storageBucket: "",
     messagingSenderId: "731375124025"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  //Create a variable to reference the database
+//Reference the database
 var rootRef = firebase.database().ref();
+//Reference the root's child
 var trainsRef = rootRef.child("trains");
+//Variable to push new train as a child of "trains"
 var newTrainRef = trainsRef.push();
+
+//Update table when values change in the server
+trainsRef.on("value", snap => {
+    $("#table-body").empty();
+    return snap.forEach(snap => {
+        appender(snap.val());
+    });
+});
 
 //On button click...
 $(".btn").on("click", function(){
@@ -31,41 +41,30 @@ $(".btn").on("click", function(){
             newArrival: newArrival(initialTime, rate)
             //timeRemaining (updates each minute)
         });
-
-        //APPEND FROM SERVER, NOT LOCAL
-        //Append new data to table
-        var trainName = $("<td>").append($("#name-input").val())
-        var trainPlace = $("<td>").append($("#place-input").val());
-        var trainRate = $("<td>").append(rate);
-        var trainTime = $("<td>").append(convertTime(initialTime));
-        var nextTrainTime = $("<td>").append(newArrival(initialTime, rate));
-        //Minutes away (updates each minute)
-        var newTrainRow = $("<tr>").append(trainName, trainPlace, trainRate, trainTime, nextTrainTime);
-        $("#table-body").append(newTrainRow);
     }
-        
 });
 
+/*FUNCTIONS USED*/
+
+//CONVERT TIME
 //Convert time into Moment object using 24-hr format
-function convertTime (timeString){
+function convertTime (timeString) {
     timestring = moment(timeString, "HH:mm A").format("HH:mm");
     return timeString;
 }
 
+//NEW ARRIVAL
 //Calculates the nextArrival time with these two parameters and the current time
-function newArrival (firstArrival, frequency){
+function newArrival (firstArrival, frequency) {
     //Calculate next arrival based on initial time/rate, get current time
     var nextArrival = moment(firstArrival, "HH:mm").add(frequency, "minutes").format("HH:mm A");
     var current = moment().format("HH:mm A");
-
     //Replace colon with decimal point for parseFloat, number comparison purposes
     var convertNext = nextArrival.replace(":", ".");
     var convertCurrent = current.replace(":", ".");
-
     //Write times as a decimal number (i.e. 13:02 will be 13.02)
     var nextDecimal = parseFloat(convertNext);
     var currentDecimal = parseFloat(convertCurrent);
-
     //If current time is ahead of next arrival time...
     while(currentDecimal > nextDecimal){
         //Continuously update until nextArrival is ahead of current time
@@ -75,4 +74,19 @@ function newArrival (firstArrival, frequency){
     }
     //Return updated arrival time
     return nextArrival;
+}
+
+//APPENDER
+//Appends train data to the HTML table body
+function appender(train) {
+    var name        = $("<td>").append(train.name);
+    var destination = $("<td>").append(train.destination);
+    var frequency   = $("<td>").append(train.frequency);
+    var initialTime = $("<td>").append(train.initialTime);
+    var newArrival  = $("<td>").append(train.newArrival);
+    //AND minutes away (duration?)
+    //Append all data to row in correct order
+    var newTrainRow = $("<tr>").append(name, destination, frequency, initialTime, newArrival);
+    //All data in correct order goes to the table body
+    $("#table-body").append(newTrainRow);
 }
